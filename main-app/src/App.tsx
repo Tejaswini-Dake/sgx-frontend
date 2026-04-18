46377c3e29c80a713a92bbff8156a37b172fb43a
Parvez Siddiqi <parvez.siddiqi@wissen.com>
2026-04-18 12:24:38 +0530

    Misc - Empty state \n lazy loading

--- main-app/src/App.tsx
+++ main-app/src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
-import { router } from './features/router';
+import { router } from '@/features/router';
 import './index.css';
 
 export default function App() {
